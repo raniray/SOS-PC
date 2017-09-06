db519aa79a866e20b6071d0c133488266994ad73
FahimaGold <df_mokhtari@esi.dz>
2017-09-06 23:00:06 +0100

    date creation update

--- newAnnonce.php
+++ newAnnonce.php
@@ -1,90 +1,94 @@
 <?php
+session_start();
 require('connexion.php');
 $db = data_base_connect();
 //Récup?ration des données du form 
 $model = !empty($_POST['model']) ? trim($_POST['model']) : null;
-$date_creation = date("l jS \of F Y h:i:s A");
+
+setlocale (LC_TIME, 'fr_FR','fra'); 
+ 
+$date_creation = strftime("%Y-%m-%d %H:%M:%S");
 $prix =  !empty($_POST['prix']) ? trim($_POST['prix']) : null;
 $processeur =  !empty($_POST['processeur']) ? trim($_POST['processeur']) : null;
 $RAM =  !empty($_POST['RAM']) ? trim($_POST['RAM']) : null;
 $sysE = !empty($_POST['sysE']) ? trim($_POST['sysE']) : null;
 $ancienn =  !empty($_POST['ancienn']) ? trim($_POST['ancienn']) : null;
 $contenuAnnonce = !empty($_POST['contenuAnnonce']) ? trim($_POST['contenuAnnonce']) : null;
-$imgAnnonce = !empty($_FILES['imgAnnonce']['name']) ? trim($_FILES['imgAnnonce']['name']) : null; 
+$duree = !empty($_POST['duree']) ? trim($_POST['duree']) : null;
+//$imgAnnonce = !empty($_FILES['imgAnnonce']['name']) ? trim($_FILES['imgAnnonce']['name']) : null; 
 //$imgAnnonce =  !empty($_POST['imgAnnonce']) ? trim($_POST['imgAnnonce']) : null;
-echo $imgAnnonce;
 $nbre_vu = 0;
-$dureeValidite = 2;
+
 $t="";
 $url = "";
-	$content ="Modèle:".$model." "."Processeur:"." ".$processeur." "."RAM:".$RAM." "."Système d'exploitation:".$sysE." "."Anciennete:".$ancienn." "."Detail:".$contenuAnnonce;
+	$content ="Modèle:".$model." "."Processeur:"." ".$processeur." "."RAM:".$RAM." "."Système d'exploitation:".$sysE." "."Anciennete:"." ".$ancienn." "."Durée de validité:".$duree." "."Detail:".$contenuAnnonce;
 
   $target_dir = "‪C:/wamp/www/SOS-PC-master1/imges/";  //Choisir là où l'image sera placée
- 
-	  
-$target_file=$imgAnnonce;
-$target_file = $target_dir . basename($_FILES["imgAnnonce"]["name"]);  //Récupérer le chemin de l'image téléchargée
-echo $target_file;
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);  //Récupérer l'extension de l'image
+ //echo $_FILES["imgAnnonce"]."LOOOOL!";
+ $imgAnnonce = !empty($_FILES["imgAnnonce"]);
 
-// Vérifier si le fichier existe déjà
-if (file_exists($target_file)) {
-    echo "Cette image existe déjà!";
-    $uploadOk = 0;
-}
-else{
-	$t=$url =basename( $_FILES["imgAnnonce"]["name"]);
-	
-}
-// Controller la taille du fichier téléchargé
-if ($_FILES["imgAnnonce"]["size"] > 500000) {
-    echo "Veuillez selectionner une image d'une taille plus petite.";
-    $uploadOk = 0;
-}
-// limité les formats permises
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Uniquement les formats JPG, JPEG, PNG & GIF files qui sont permises.";
-    $uploadOk = 0;
-}
-
-if ($uploadOk == 0) {
-    echo "Oups! Votre image n'a pas pu être  téléchargée!.";
-} else {
-	
-    if (move_uploaded_file($_FILES["imgAnnonce"]["tmp_name"], $target_file)) {
-		$url =basename( $_FILES["imgAnnonce"]["name"]);
-		$base = 'http://localhost/SOS-PC-master1/home.php';
-$data = array(
-    'url' => $url,
-);
-
-$info = $base . '?' . http_build_query($data);
-header("Location: $info");
-       // echo "The file ". basename( $_FILES["imgAnnonce"]["name"]). " has been uploaded.";
-		//"<img  id='logo' src='../../img/>"
-		
-		echo $url;
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
- 
+	  // echo "File Name: " . $_FILES["imgAnnonce"]["name"] . "<br>";
+//if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Check if file was uploaded without errors
+    if(isset($_FILES["imgAnnonce"]) && $_FILES["imgAnnonce"]["error"] == 0){
+        $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+        $filename = $_FILES["imgAnnonce"]["name"];
+        $filetype = $_FILES["imgAnnonce"]["type"];
+        $filesize = $_FILES["imgAnnonce"]["size"];
     
-/*$stmt = $db->prepare("INSERT INTO annonce_ (annoncePic,annonceContent,dateCreation,prix,nombreDeVues,durreeValidite) VALUES (:img,:annonceContenu,:dateC,:prixA,:nbreVues,:dureeValid)");
+        // Verify file extension
+        $ext = pathinfo($filename, PATHINFO_EXTENSION);
+        if(!array_key_exists($ext, $allowed)) die("Error: Please select a valid file format.");
+    
+        // Verify file size - 5MB maximum
+        $maxsize = 5 * 1024 * 1024;
+        if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
+    
+        // Verify MYME type of the file
+        if(in_array($filetype, $allowed)){
+            // Check whether file exists before uploading it
+            if(file_exists("upload/" . $_FILES["photo"]["name"])){
+                echo $_FILES["imgAnnonce"]["name"] . " is already exists.";
+            } else{
+                move_uploaded_file($_FILES["imgAnnonce"]["tmp_name"], "upload/" . $_FILES["imgAnnonce"]["name"]);
+                echo "Your file was uploaded successfully.";
+            } 
+        } else{
+            echo "Error: There was a problem uploading your file. Please try again."; 
+        }
+    } else{
+       // echo "Error: " . $imgAnnonce["error"];
+    }
+//}
+ 
+   $email =  $_SESSION['mail'];
+ $stmt1 = $db->prepare("SELECT idUser FROM User_ WHERE mailUser=:mail");
+ $stmt1->bindValue(':mail',$email);
+ try{
+    $stmt1->execute();	 
+    while($row = $stmt1->fetch(PDO::FETCH_ASSOC)) 
+{
+  $ID = $row['idUser'];
+
+}
+}catch (Exception $e) {
+        echo $e->getMessage();
+      }
+	  
+$stmt = $db->prepare("INSERT INTO annonce_ (annoncePic,annonceContent,dateCreation,prix,nombreDeVues,durreeValidite,idUser) VALUES (:img,:annonceContenu,:dateC,:prixA,:nbreVues,:dureeValid,:idU)");
 $stmt->bindValue(':annonceContenu', $content);
 $stmt->bindValue(':dateC', $date_creation);
 $stmt->bindValue(':prixA', $prix);
 $stmt->bindValue(':nbreVues', $nbre_vu);
-$stmt->bindValue(':dureeValid', $dureeValidite);
+$stmt->bindValue(':dureeValid', $duree);
 $stmt->bindValue(':img', $url);
+$stmt->bindValue(':idU', $ID);
 try{
     $stmt->execute();	 
-    // echo "votre annonce a ete cree!";
+     echo "votre annonce a ete cree!";
 }catch (Exception $e) {
         echo $e->getMessage();
-      }*/
+      }
 
 	
  
